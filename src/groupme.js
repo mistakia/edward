d1ff673531a5c612a15d8f9e0b95a13b745e2e2b
mistakia <1823355+mistakia@users.noreply.github.com>
2021-02-06 12:44:30 -0500

    refactor: groupme message parsing

--- src/groupme.js
+++ src/groupme.js
@@ -1,7 +1,14 @@
 const GroupMe = require('groupme')
+const debug = require('debug')
 const API = GroupMe.Stateless
 
+const Edward = require('./index')
+const constants = require('../constants')
 const config = require('../config')
+const { parseMessage, sendDirectMessage, sendGroupMessage } = require('./utils')
+
+const log = debug('groupme')
+const edward = new Edward(config.seed)
 
 const incoming = new GroupMe.IncomingStream(
   config.groupme.ACCESS_TOKEN,
@@ -11,27 +18,118 @@ const incoming = new GroupMe.IncomingStream(
 
 // This waits for the IncomingStream to complete its handshake and start listening.
 // We then get the bot id of a specific bot.
-incoming.on('connected', function() {
-  console.log("[IncomingStream 'connected']")
+incoming.on('connected', () => {
+  log("[IncomingStream 'connected']")
 })
 
 // This waits for messages coming in from the IncomingStream
 // If the message contains @BOT, we parrot the message back.
-incoming.on('message', function(msg) {
-  console.log("[IncomingStream 'message'] Message Received")
-  console.log(msg)
+incoming.on('message', async (msg) => {
+  const validTypes = ['line.create', 'direct_message.create']
+  if (!validTypes.includes(msg.data.type)) return
+
+  if (!msg.data.subject.text) {
+    return
+  }
+
+  log("[IncomingStream 'message'] Message Received")
+  log(msg)
+
+  /* if (message.group_id !== config.groupme.GROUP_ID) {
+   *   return
+   * }
+   */
+
+  const parsed = parseMessage(msg.data.subject.text)
+  if (!parsed.valid) {
+    if (parsed.message === constants.MISSING_KEYWORD) return
+
+    switch (parsed.message) {
+      case constants.INVALID_ADDRESS:
+      case constants.MISSING_ADDRESS:
+        await sendDirectMessage({
+          type: constants.GROUPME,
+          message: parsed.message,
+          userId: msg.data.subject.user_id
+        })
+        break
+
+      case constants.MISSING_AMOUNT:
+      case constants.INVALID_AMOUNT: {
+        const groupId = msg.data.subject.group_id
+        if (!groupId) {
+          // TODO - send notification
+          return
+        }
+
+        await sendGroupMessage({
+          type: constants.GROUPME,
+          message: parsed.message,
+          groupId: groupId
+        })
+        break
+      }
+    }
+
+    return
+  }
+
+  log(`[groupme][${parsed.command}]`)
+  switch (parsed.command) {
+    case 'register': {
+      await edward.register({
+        userId: msg.data.subject.user_id,
+        type: constants.GROUPME,
+        address: parsed.params.address
+      })
+      break
+    }
+
+    case 'rain': {
+      const groupId = msg.data.subject.group_id
+      if (!groupId) {
+        // TODO - send notification
+        return
+      }
+
+      const res = await API.Groups.show.Q(config.groupme.ACCESS_TOKEN, groupId)
+      log(res)
+      // const blocks = await edward.rain({ userId, type: constants.GROUPME, to })
+      break
+    }
+
+    case 'tip': {
+      // get all mentions
+      log(msg.data.subject.attachments)
+      const blocks = await edward.tip({
+        senderId: msg.data.subject.user_id,
+        type: constants.GROUPME,
+        receiverIds: []
+      })
+      log(blocks)
+      break
+    }
+
+    case 'info':
+      break
+
+    case 'stats':
+      break
+
+    case 'help':
+      break
+  }
 })
 
 // This listens for the bot to disconnect
-incoming.on('disconnected', function() {
-  console.log("[IncomingStream 'disconnect']")
+incoming.on('disconnected', () => {
+  log("[IncomingStream 'disconnect']")
   // TODO - reconnect
 })
 
 // This listens for an error to occur on the Websockets IncomingStream.
-incoming.on('error', function() {
-  var args = Array.prototype.slice.call(arguments)
-  console.log("[IncomingStream 'error']", args)
+incoming.on('error', (message, payload) => {
+  log("[IncomingStream 'error']", message, payload)
   // TODO - reconnect
 })
 
--- src/server.js
+++ src/server.js
@@ -2,22 +2,18 @@ const express = require('express')
 const morgan = require('morgan-debug')
 const bodyParser = require('body-parser')
 const debug = require('debug')
-const GroupMe = require('groupme')
 
 const groupmePubsub = require('./groupme')
 const Edward = require('./index')
 const config = require('../config')
-const constants = require('../constants')
 const pkg = require('../package.json')
-const { parseMessage, sendDirectMessage, sendGroupMessage } = require('./utils')
 
-const API = GroupMe.Stateless
 const app = express()
 
 app.locals.log = debug('server')
 app.locals.edward = new Edward(config.seed)
 app.locals.groupme = groupmePubsub
-if (process.env.NODE_ENV !== 'test') debug.enable('server*')
+if (process.env.NODE_ENV !== 'test') debug.enable('server*,groupme')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -26,84 +22,6 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
-})
-
-app.post('/groupme', async (req, res) => {
-  const message = req.body
-
-  if (!message.text) {
-    return
-  }
-
-  if (message.group_id !== config.groupme.GROUP_ID) {
-    return
-  }
-
-  const msg = parseMessage(message.text)
-  if (!msg.valid) {
-    if (msg.message === constants.MISSING_KEYWORD) return
-
-    switch (msg.message) {
-      case constants.INVALID_ADDRESS:
-      case constants.MISSING_ADDRESS:
-        await sendDirectMessage({
-          type: constants.GROUPME,
-          message: msg.message,
-          userId: message.user_id
-        })
-        break
-
-      case constants.MISSING_AMOUNT:
-      case constants.INVALID_AMOUNT:
-        await sendGroupMessage({
-          type: constants.GROUPME,
-          message: msg.message,
-          groupId: message.group_id
-        })
-        break
-    }
-
-    return
-  }
-
-  app.locals.log(`[groupme][${msg.command}]`)
-  switch (msg.command) {
-    case 'register': {
-      await app.locals.register({
-        userId: message.user_id,
-        type: constants.GROUPME,
-        address: msg.params.address
-      })
-      break
-    }
-
-    case 'rain': {
-      const res = await API.Groups.show.Q(config.groupme.ACCESS_TOKEN, message.group_id)
-      console.log(res)
-      // const blocks = await app.locals.rain({ userId, type: constants.GROUPME, to })
-      break
-    }
-
-    case 'tip': {
-      // get all mentions
-      const blocks = await app.locals.tip({
-        senderId: message.user_id,
-        type: constants.GROUPME,
-        receiverIds: []
-      })
-      console.log(blocks)
-      break
-    }
-
-    case 'info':
-      break
-
-    case 'stats':
-      break
-
-    case 'help':
-      break
-  }
 })
 
 app.get('/', (req, res) => {
